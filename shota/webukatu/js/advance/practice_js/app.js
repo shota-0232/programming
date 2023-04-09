c6c43fd90f65f8bd80fa4d5796164bd6dd83984a
s_hiratsuka <shiratsuka@cruxsystem.co.jp>
2023-04-09 22:34:16 +0900

    lesson8

---
+++ shota/webukatu/js/advance/practice_js/app.js
@@ -0,0 +1,11 @@
+$(function() {
+    
+    $('.js-form-validate').on('keyup', function() {
+        if($(this).val()) {
+            $('.js-disabled-submit').prop('disabled', false);
+        } else {
+            $('.js-disabled-submit').prop('disabled', true);
+
+        }
+    })
+})
